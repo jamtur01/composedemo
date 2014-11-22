aca137a881cdf80150a56afd4b21dffcb6b27495
root <root@docker.lovedthanlost.net>
2014-11-22 18:28:30 -0500

    first commit

---
+++ myapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = 'a867d71334dcfedd5e0c3b12375ca01e52161cb4d2fa2ff453d8c861276df74cfb1d2a38af5cb845ec47a149d3baa13f76be325b13d8a1d68ca56e243954f183'
